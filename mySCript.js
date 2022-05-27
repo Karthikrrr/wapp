763e6e7590d27f7627f4f89c24e10b58fc78b2e4
karthikrrr <karthikrkarthi6@gmail.com>
2022-05-27 12:08:23 +0530

    new

---
+++ mySCript.js
@@ -0,0 +1,58 @@
+const months = ['Jan', 'Feb', 'Mаr', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Ocт', 'Nov', 'Dec'];
+const count = 3;
+const data = [];
+
+months.forEach(month => {
+  const values = [];
+
+  for (var i = 0; i < count; i++) {
+    values.push({ key: i, value: Math.round(Math.random() * 100) / 100 });
+  }
+  data.push({ month, values });
+});
+
+const width = 600;
+const height = 400;
+const margin = { top: 25, right: 25, bottom: 25, left: 25 };
+
+const svg = d3.select('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', `translate(${margin.left},${margin.top})`);
+
+const xScale = d3.scaleBand().domain(months).range([0, width]).padding(0.1);
+
+const xInnerDomain = data[0].values.keys();
+const xInnerScale = d3.scaleBand().domain(xInnerDomain).rangeRound([0, xScale.bandwidth()]);
+
+const yDomain = [
+  d3.min(data, d => d3.min(d.values.map(v => v.value))),
+  d3.max(data, d => d3.max(d.values.map(v => v.value)))
+];
+const yScale = d3.scaleLinear().domain(yDomain).range([height, 0]);
+
+const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+svg.append('g')
+  .attr('transform', `translate(0,${height})`)
+  .call(d3.axisBottom(xScale));
+svg.append('g')
+  .call(d3.axisLeft(yScale));
+
+const barGroup = svg.selectAll('.group')
+  .data(data)
+  .enter()
+  .append('g')
+  .attr('class', 'group')
+  .attr('transform', d => `translate(${xScale(d.month)},0)`);
+
+barGroup.selectAll('rect')
+  .data(d => d.values)
+  .enter()
+  .append('rect')
+  .attr('width', xInnerScale.bandwidth())
+  .attr('x', d => xInnerScale(d.key))
+  .style('fill', d => colorScale(d.key))
+  .attr('y', d => yScale(d.value))
+  .attr('height', d => height - yScale(d.value));
